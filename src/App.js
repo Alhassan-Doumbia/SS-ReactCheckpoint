610d832bc39d9326578276cc7abdce8a170128c2
Doumbia Alhassan <hassan4cod@gmail.com>
2024-05-26 09:30:27 +0000

    created a navbar using Booststrap classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './component-style/style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+render() {
+    return(
+        <>
+            <nav className="bg-dark d-flex align-items-center" >
+                <p className="fw-bold text-light fs-4">My logo</p>
+                <ul className="d-flex fs-6 text-light-emphasis gap-4">
+                    <li className="li-element">home</li>
+                    <li className="li-element">About</li>
+                    <li className="li-element">contact</li>
+                </ul>
+        </nav>
+        </>
+    )
 }
-
-export default App;
+}
+export default App
